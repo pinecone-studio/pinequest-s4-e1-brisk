0e5912bc83d5eab693ccd0fe55d4e323fd129b1e
Batbileg <Batblg247@gmail.com>
2026-06-06 15:47:54 +0800

    Some cheamges (#122)
    Co-authored-by: Batbileg Surlegbayar - G <25LP3625@administrators-iMac-4e-403.local>
    Co-authored-by: Danny Otgontsetseg <dnioko0412@gmail.com>

--- server/src/routes/users/user.routes.ts
+++ server/src/routes/users/user.routes.ts
@@ -2,12 +2,13 @@ import { Hono } from "hono";
 import { getUserById } from "../../controllers/users/get-user";
 import { getUsers } from "../../controllers/users/get-users";
 import { createUser } from "../../controllers/users/post-user";
+import { syncUser } from "../../controllers/users/post-sync-user";
 import { Bindings } from "../../lib/common/types";
 
 const userRoutes = new Hono<{ Bindings: Bindings }>();
 
 userRoutes.get("/", getUsers);
 userRoutes.post("/", createUser);
-userRoutes.get("/:id", getUserById);
+userRoutes.post("/sync", syncUser);
 
 export default userRoutes;
